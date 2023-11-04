ed895933ce11d177a4325e4beee6afc5e130b9aa
Elias Moura <elias.smourayt@gmail.com>
2023-11-04 19:50:26 -0300

    testando componente

--- src/components/List/item/index.tsx
+++ src/components/List/item/index.tsx
@@ -1,6 +1,8 @@
+import { ITask } from '../../../types/tasks';
 import style from '../list.module.scss';
 
-export default function Item({task, time}:{ task: string, time: string}){
+export default function Item({task, time, isSelected ,isCompleted, id}: ITask){
+    console.log({task, time, isSelected ,isCompleted, id});
     return (
     <li className={style.item}>
         <h3>{task}</h3>
