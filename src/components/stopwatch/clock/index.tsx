c46a3399343e2079cd671b95c611e73685fc7641
Elias Moura <elias.smourayt@gmail.com>
2023-11-06 13:53:54 -0300

    deixando o relógio dinamico.

--- src/components/stopwatch/clock/index.tsx
+++ src/components/stopwatch/clock/index.tsx
@@ -1,13 +1,24 @@
 import style from './clock.module.scss';
 
-export default function Clock() {
+interface Props {
+    time: number | undefined
+}
+
+export default function Clock({ time = 0}: Props) {
+    const secondsInMinutes = 60;
+    const minutes = String(Math.floor(time / secondsInMinutes));
+    const seconds = String(time % secondsInMinutes);
+
+    const [minutesTen, minutesUnit] = minutes.padStart(2, '0');
+    const [secondsTen, secondsUnit] = seconds.padStart(2, '0');
+
     return (
         <>
-            <span className={style.clockNumber}>0</span>
-            <span className={style.clockNumber}>0</span>
+            <span className={style.clockNumber}>{minutesTen}</span>
+            <span className={style.clockNumber}>{minutesUnit}</span>
             <span className={style.clockDivision}>:</span>
-            <span className={style.clockNumber}>0</span>
-            <span className={style.clockNumber}>0</span>
+            <span className={style.clockNumber}>{secondsTen}</span>
+            <span className={style.clockNumber}>{secondsUnit}</span>
         </>
     )
 }
